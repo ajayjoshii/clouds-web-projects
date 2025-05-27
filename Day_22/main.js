97a15af08ed5a873986719f35a3ba1f85d496493
ajayjoshii <jajay7941@gmail.com>
2025-05-27 18:21:39 +0545

    ajay

---
+++ Day_22/main.js
@@ -0,0 +1,113 @@
+// // let age = 20;
+// // if(age <= 10){
+
+// // console.log("0");
+// // }
+// // else if (age>=10 || age <=17){
+// //     console.log("2");
+// // }else
+// // {
+// //     console.log("1");
+// // }
+// console.log(new Date());
+// let dayNumber = 8; // you can change this value to test different days
+// let dayName;
+
+// switch (dayNumber) {
+//   case 1:
+//     dayName = "Sunday";
+//     break;
+//   case 2:
+//     dayName = "Monday";
+//     break;
+//   case 3:
+//     dayName = "Tuesday";
+//     break;
+//   case 4:
+//     dayName = "Wednesday";
+//     break;
+//   case 5:
+//     dayName = "Thursday";
+//     break;
+//   case 6:
+//     dayName = "Friday";
+//     break;
+//   case 7:
+//     dayName = "Saturday";
+//     break;
+//   default:
+//     dayName = "Invalid day number!";
+// }
+
+// console.log("Today is: " + dayName);
+
+// var currentDate = new Date();
+
+// var dayNumber = currentDate.getDay();
+
+// var dayName;
+
+// switch (dayNumber) {
+//     case 0:
+//         dayName = "Sunday";
+//         break;
+//     case 1:
+//         dayName = "Monday";
+//         break;
+//     case 2:
+//         dayName = "Tuesday";
+//         break;
+//     case 3:
+//         dayName = "Wednesday";
+//         break;
+//     case 4:
+//         dayName = "Thursday";
+//         break;
+//     case 5:
+//         dayName = "Friday";
+//         break;
+//     case 6:
+//         dayName = "Saturday";
+//         break;
+//     default:
+//         dayName = "Invalid day";
+// }
+
+// console.log("Day of the week: " + dayName);
+// const day = new Date().getDay()
+// switch (day){
+//     case 0:
+//     console.log("Sunday")
+//     break;
+//     case 1:
+//     console.log("Monday")
+//     break;
+//     case 2:
+//     console.log("Tuesday")
+//     break;
+//     case 3:
+//     console.log("Wednesday")
+//     break;
+//     case 4:
+//     console.log("Thursday")
+//     break;
+//     case 5:
+//     console.log("Friday")
+//     break;
+//     case 6:
+//     console.log("Saturday")
+//     break;
+//     default:
+//     console.log("invalid day")
+//     break;
+
+// }
+let a=1;
+while(a<=5)
+{
+    a++;
+}
+do{
+    console.log(a);
+    a++;
+}while(a<=10)
